9f68fff36fd7f34915ec062711d477e96c73773e
95 <117367301+95@users.noreply.github.com>
2022-12-05 12:57:40 -0800

    Add files via upload
    Controller for LoginPage

---
+++ Interface/LoginMainController.java
@@ -0,0 +1,20 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField username1;
+
+    @FXML
+    private Label wrongLogin;
+
+}
