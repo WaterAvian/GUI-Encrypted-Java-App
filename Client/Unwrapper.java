896ec241e5d6c9628c94a0d7908d36f86add1925
aqqmin <n.quinnallen@gmail.com>
2022-12-07 14:16:15 +0000

    Just rearranging into directorys for ease of nav

---
+++ Client/Unwrapper.java
@@ -0,0 +1,45 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class Unwrapper {
+
+    public byte firstByte;
+    public byte secondByte;
+    public int length;
+    public byte[] payload;
+    
+    Unwrapper(BufferedInputStream buffer) throws IOException
+    {
+        this.firstByte = ByteBuffer.allocate(4).putInt(buffer.read()).get(0);  System.out.println("first"+firstByte); //gets byte at 0 of returned int (4 bytes)
+        this.secondByte = ByteBuffer.allocate(4).putInt(buffer.read()).get(0);  System.out.println("second"+secondByte);
+        this.length = getLength(buffer); System.out.println("lenth"+length);
+        this.payload = getPayload(buffer, length); System.out.println("payload"+payload);
+    }
+
+    private int getLength(BufferedInputStream buffer) throws IOException
+    {
+        byte[] array = new byte[4];
+        for(int i=0; i<4; i++)
+        {
+            array[i] = ByteBuffer.allocate(4).putInt(buffer.read()).get(0);
+        } 
+
+        int test = ByteBuffer.wrap(array).getInt();
+        System.out.println(test);
+        return test;
+    }
+
+    private byte[] getPayload(BufferedInputStream buffer, int length) throws IOException
+    {
+        byte[] array = new byte[length];
+        for(int i=0; i<length; i++)
+        {
+            array[i] = ByteBuffer.allocate(4).putInt(buffer.read()).get(0);
+        }
+        return array;
+    }
+
+
+
+}
